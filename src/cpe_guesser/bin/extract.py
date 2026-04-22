28efe6b5da0f82b5cfb5b7fbe5efe75d50c60db1
qjerome <qjerome@rawsec.lu>
2026-04-22 21:20:22 +0200

    feat(cli): generic CPE extract script

---
+++ src/cpe_guesser/bin/extract.py
@@ -0,0 +1,29 @@
+import argparse
+import sys
+
+from cpe_guesser.cpeimport.reader.generic import GenericCPEReader
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Extract unique CPE based on regex from any input text"
+    )
+
+    parser.add_argument(
+        "CPE_FILES", type=str, nargs="+", help="Text file containing CPE data"
+    )
+
+    args = parser.parse_args()
+
+    for cpe_file in args.CPE_FILES:
+        if cpe_file == "-":
+            reader = GenericCPEReader("stdin", text_io=sys.stdin)
+        else:
+            reader = GenericCPEReader(cpe_file)
+
+        for cpe in reader.find_all_cpe_str():
+            print(f"{cpe}")
+
+
+if __name__ == "__main__":
+    main()
